81cd3d5ad1644b04a109cc4d03ef5e754674fa94
sanaehururu1200 <sanaehururu1200@gmail.com>
2020-04-20 09:18:20 +0900

    Update do-not-open-twitter.user.js
    Change header

--- do-not-open-twitter.user.js
+++ do-not-open-twitter.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name		DO NOT OPEN TWITTER
-// @description	Redirect to SoundCloud
+// @description	Redirect to Gmail
 // @version		0.1.0
 // @include		https://twitter.com/*
 // @include		https://tweetdeck.twitter.com/*
-// @author		yanorei32
-// @website		https://github.com/Yanorei32/do-not-open-twitter
-// @namespace	http://yano.teamfruit.net/~rei/
-// @updateURL	https://github.com/Yanorei32/do-not-open-twitter/do-not-open-twitter.user.js
+// @author		sanaehururu1200(original:yanorei32)
+// @website		https://github.com/sanaehururu1200/do-not-open-twitter (original:https://github.com/yanorei32/do-not-open-twitter)
+// @namespace	https://twitter.com/sanaehururu2100 (original:http://yano.teamfruit.net/~rei/)
+// @updateURL	https://github.com/sanaehururu1200/do-not-open-twitter/do-not-open-twitter.user.js
 // @license		The Unlicense
 // @grant		none
 // @run-at		document-start
